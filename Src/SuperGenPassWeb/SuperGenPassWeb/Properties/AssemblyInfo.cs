73692ca365891600ec022bcbfafe5bc2cd246192
Kernel Man <arseny999@gmail.com>
2013-03-10 18:28:02 +0400

    Internal release. 1.0.2.0
    Version 1.0.2.0.

---
+++ Src/SuperGenPassWeb/SuperGenPassWeb/Properties/AssemblyInfo.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Resources;
+
+// Управление общими сведениями о сборке осуществляется с помощью следующего 
+// набора атрибутов. Измените значения этих атрибутов для изменения
+// сведений о сборке.
+[assembly: AssemblyTitle("SuperGenPassWeb")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SuperGenPassWeb")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Если для ComVisible установить значение false, типы в этой сборке не будут поддерживаться 
+// COM-компонентами.  При необходимости доступа к какому-либо типу в этой сборке 
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если данный проект видим для COM
+[assembly: Guid("7bea138d-f5fe-4573-a8f2-d14d3f5112fc")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//      Номер построения
+//      Редакция
+//
+// Можно указать все значения или задать для номеров редакции и построения значения по умолчанию 
+// с помощью символа '*', как показано ниже:
+[assembly: AssemblyVersion("1.0.2")]
+[assembly: AssemblyFileVersion("1.0.2")]
+[assembly: NeutralResourcesLanguageAttribute("ru-RU")]
